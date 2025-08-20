a41f1a415f10821edaf476af3a87947b17c5ebaf
pachimole <salvador.zoe@hotmail.com>
2025-08-20 10:28:01 -0300

    primera subida

---
+++ src/entity/Clase2.java
@@ -0,0 +1,17 @@
+package entity;
+
+public class Clase2 {
+    public static void main(String[] args) {
+
+        String trayectos ="Programador/a";
+        String modulo = "Programacion Orientada a Objetos";
+        int carga_horaria = 150;
+        double tiempo_Clase= 3.2;
+
+        System.out.println("indormacion del trayecto: \n");
+
+
+
+
+    }
+}
---
+++ src/entity/Tutoriales.java
@@ -0,0 +1,40 @@
+package entity;
+
+public class Tutoriales {
+    public static void main(String[] args) {
+
+        /*
+        //TIPOS DE DATOS PRIMITIVOS
+        int = numero entero
+        double = numero con coma
+        char = un caracter ej: a
+        bolean = verdadero y falso
+
+        //
+
+
+
+
+        //CONTAR CARACTERES
+        //contar cuantas letras 'a' hay en ej
+        String ej = "Holaa";
+        System.out.println(ej.length());
+
+        int contador_letras = 0;
+        for(int i=0 ; i<ej.length() ; i++){
+            if(ej.charAt(i) == a){
+                contador_letras ++;
+            }
+        }
+
+        System.out.println("la cantidad de 'a' es: " + contador_letras);
+
+
+
+        //int (redondea para abajo)
+        //Math.round (redondea para arriba)
+        */
+
+    }
+
+}
