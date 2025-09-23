00d87669da64e7a6cf624d928508084c3474d91d
pachimole <salvador.zoe@hotmail.com>
2025-09-23 11:54:51 -0300

    array

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,72 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import entity.BilleteraVirtual;
+
+import java.util.ArrayList;
+
 public class Main {
+    //---------------------------------------------------------------------------------------------
+    //crear arrayList
+    static ArrayList<BilleteraVirtual> billeteras = new ArrayList<>();
+
+    //---------------------------------------------------------------------------------------------
+    //agregar villeteras, con un limite de 5.
+    public static void agregarBilletera (BilleteraVirtual billeteraVirtual){
+        if(billeteras.size() == 5){ //--> crea un limite de billeteras a agregar.
+            System.out.println("se paso el limite permitido. ");
+        }else if(billeteras.contains(billeteraVirtual)){  //--> verifica que la billetera no este registrada.
+            System.out.println("ya esta registrada en el sistema.");
+        }else{
+            billeteras.add(billeteraVirtual); //--> agrega la billetera
+            System.out.println("la Billetera "+ billeteraVirtual +" fue registrada.");
+        }
+    }
+    //---------------------------------------------------------------------------------------------
+    //Eliminar
+    public void eliminarBilletera(BilleteraVirtual billeteraVirtual){
+        if(billeteras.contains(billeteraVirtual)) billeteras.remove(billeteraVirtual);
+        else {
+            System.out.println("la Billetera no esta en la lista.");
+        }
+    }
+
+    //---------------------------------------------------------------------------------------------
+    //consultar
+    public static void consultarBilletera(){
+        for (BilleteraVirtual billeteraVirtual:billeteras);
+    }
+
+    //---------------------------------------------------------------------------------------------
     public static void main(String[] args) {
+
+        BilleteraVirtual billetera1 = new BilleteraVirtual(12345678,"Salvador",42470074,3000.0);
+        BilleteraVirtual billetera2 = new BilleteraVirtual(87654321,"Pepito",12345678,5000.0);
+
+        //Consultar num de cuenta
+        System.out.println(billetera1.getNumCuenta());
+
+        //Consultar saldo
+        System.out.println(billetera1.saldoActual());
+
+        //Retirar saldo
+        System.out.println(billetera1.retirarSaldo(1000.0));
+
+        //Recibir saldo
+        System.out.println(billetera1.recibirSaldo(100.0));
+
+        //Transferir saldo
+        System.out.println(billetera1.transferirSaldo(1000.0,87654321));
+
+        //Ver limite de extraccion
+        System.out.println(billetera1.getLimiteExtraccion());
+
+        //cambiar limite de extraccion
+
+
+        //---------------------------------------------------------------------------------------------
+        //agregar cuentas a ArrayList
+
+        agregarBilletera(billetera1);
+        agregarBilletera(billetera2);
+
 
     }
 
---
+++ src/entity/BilleteraVirtual.java
@@ -0,0 +1,66 @@
+package entity;
+
+public class BilleteraVirtual {
+
+    private Integer numCuenta;
+    private String nombre;
+    private Integer dni;
+    private Double saldo;
+    private static final Double COMISION= 0.05;
+    private static Double limiteExtraccion= 50000.0;
+
+    //-----------------------------------------------------------------------------------------------
+    //Constructor
+    public BilleteraVirtual ( Integer numCuenta, String nombre, Integer dni, Double saldo){
+        this.numCuenta=numCuenta;
+        this.nombre=nombre;
+        this.dni=dni;
+        this.saldo=saldo;
+    }
+
+    //-----------------------------------------------------------------------------------------------
+    //CONSULTAS
+    //consutlar limite de extraccion
+    public Double getLimiteExtraccion() {return  this.limiteExtraccion; }
+    //consultar numero de cuenta
+    public Integer getNumCuenta(){
+        return this.numCuenta;
+    }
+
+    //consultar saldo
+    public Double saldoActual(){
+        return this.saldo;
+    }
+
+    //-----------------------------------------------------------------------------------------------
+    //retirar saldo
+    public String retirarSaldo (Double num){
+        if (num <= saldo && num<=limiteExtraccion){
+            this.saldo = this.saldo - num;
+            return "Retiro exitoso, tu saldo actual es de: " + saldo;
+        }else return "Fondos insuficientes.";
+    }
+
+    //recibir saldo
+    public String recibirSaldo ( Double monto ){
+        this.saldo = this.saldo + (monto-(monto*COMISION));
+        return "Saldo recibido: " + (monto-(monto*COMISION)) + " Saldo actual: " + this.saldo;
+    }
+
+    //transferir saldo
+    public String transferirSaldo (Double num, Integer numCuenta){
+        if (num <= saldo && num<=limiteExtraccion) {
+            this.saldo = this.saldo - num;
+            return "Transferencia exitosa, tu saldo actual es de: " + saldo;
+        }else return "Fondos insuficientes.";
+    }
+    //-----------------------------------------------------------------------------------------------
+    // Cambiar limite de extraccion
+
+    public static void setLimiteExtraccion(Double nuevoLimite){
+        limiteExtraccion = nuevoLimite;
+    }
+
+
+
+}
